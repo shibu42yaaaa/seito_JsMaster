a85dcab80426981d431856cd57fabcf4a6eeccb6
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-01-02 10:24:17 +0900

    画面に正誤表示

--- quiz_game/app.js
+++ quiz_game/app.js
@@ -15,3 +15,12 @@ document.getElementsByTagName('button')[0].textContent = answers[0];
 document.getElementsByTagName('button')[1].textContent = answers[1];
 document.getElementsByTagName('button')[2].textContent = answers[2];
 document.getElementsByTagName('button')[3].textContent = answers[3];
+
+// ボタンがクリックされたら生後判定
+document.getElementsByTagName('button')[1].addEventListener('click', () => {
+    if(correct === document.getElementsByTagName('button')[1].textContent){
+      window.alert('正解！');
+    } else {
+      window.alert('残念。。');
+    }
+});
