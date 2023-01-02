ce9e6c193e49f091a255120137c3c120de89df6d
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-01-02 12:10:18 +0900

    リファクタリング３

--- quiz_game/app.js
+++ quiz_game/app.js
@@ -9,12 +9,12 @@ const answers = [
 ];
 const correct = '東京ヤクルトスワローズ';
 const $button = document.getElementsByTagName('button'); 
+const buttonLength = $button.Length;
 
 const setupQuiz = () => {
     document.getElementById('js-question').textContent = question; 
     
     let buttonIndex = 0;
-    let buttonLength = $button.length;
     while(buttonIndex < buttonLength){
       $button[buttonIndex].textContent = answers[buttonIndex];
       buttonIndex++;
@@ -32,18 +32,11 @@ const clickHandler = (e) =>{
 };
 
 // ボタンがクリックされたら生後判定
-$button[0].addEventListener('click', (e) => {
+let handlerIndex = 0;
+while (handlerIndex < buttonLength) {
+  $button[handlerIndex].addEventListener('click', (e) => {
     clickHandler(e);
-});
+  });
+  handlerIndex++;
+}
 
-$button[1].addEventListener('click', (e) => {
-    clickHandler(e);
-});
-
-$button[2].addEventListener('click', (e) => {
-    clickHandler(e);
-});
-
-$button[3].addEventListener('click', (e) => {
-    clickHandler(e);
-});
