ff1cdc1398d9cfc90c7203daa360a10c0d20d84c
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-01-02 11:41:25 +0900

    ボタン名のリファクタリング

--- quiz_game/app.js
+++ quiz_game/app.js
@@ -8,15 +8,20 @@ const answers = [
   '中日ドラゴンズ'
 ];
 const correct = '東京ヤクルトスワローズ';
-
-document.getElementById('js-question').textContent = question;
-
 const $button = document.getElementsByTagName('button'); 
 
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3];
+const setupQuiz = () => {
+    document.getElementById('js-question').textContent = question; 
+    
+    let buttonIndex = 0;
+    let buttonLength = $button.length;
+    while(buttonIndex < buttonLength){
+      $button[buttonIndex].textContent = answers[buttonIndex];
+      buttonIndex++;
+    }
+}
+// 定義した値を呼ぶ
+setupQuiz();
 
 // ボタンがクリックされたら生後判定
 $button[0].addEventListener('click', () => {
